ed78be29e01f00b1679316175359208dee416162
markr211 <90472962+markr211@users.noreply.github.com>
2022-03-30 07:14:02 +0100

    PS4; begin

---
+++ problemSets/PS4/my_answers/PS4_mark_roche.R
@@ -0,0 +1,24 @@
+######## PROBLEM SET 4 #######
+
+#### Mark Roche ####
+
+### PROBLEM 1 ####
+
+# We’re interested in modeling the historical causes of infant mortality. 
+# We have data from 5641 first-born in seven Swedish parishes 1820-1895. 
+# Using the ”infants” dataset in the eha library, fit a Cox Proportional Hazard model 
+# using mother’s age and infant’s gender as covariates. Present and interpret the output.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
